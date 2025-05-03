c4b0a0baf41b4f6a1b9524628f26e631efa48de1
somerandomenumbers <dextanium@outlook.com>
2025-05-03 10:12:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Supremacy AI
+
+Template AI bot for the [Supremacy](https://github.com/nvaytet/supremacy) game
---
+++ __init__.py
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: BSD-3-Clause
+
+from .simple_ai import CREATOR, PlayerAi
+# from .barebones_ai import CREATOR, PlayerAi
+
+__all__ = ["CREATOR", "PlayerAi"]
